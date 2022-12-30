cd6c77998a8d02369dec379486660084ef1b68da
Gary Wolfe <avidgamefan@yahoo.com>
2022-12-29 23:33:20 -0500

    SD 2.x
    Updated to latest Easy Diffusion Beta branch as of 12/29/2022.  Support changes to data structures.  Added guidance increase.  Patch potential scalupbig bug (prevent runaway).

--- scaleupMAX.plugin.js
+++ scaleupMAX.plugin.js
@@ -1,6 +1,6 @@
 /**
  * Scale Up MAX
- * v.1.01, last updated: 11/26/2022
+ * v.1.02, last updated: 12/29/2022
  * By Gary W.
  * 
  * Scaling up in size, to the maximum resolution of your video card, while maintaining 
@@ -16,13 +16,10 @@
    These values work (usually) for the Nvidia 2060 Super with 8GB VRAM. 
    If you go too large, you'll see "Error: CUDA out of memory". 
  */
-var maxTotalResolution = 1024 * 1024;  //put max resolution (without turbo mode) here
-//var maxTotalResolution = 1536 * 1024;  //put max resolution (without turbo mode) here
-var maxTurboResolution = 1024 * 1024;  //put max turbo resolution here, max possible size when turbo is on
-//var maxTurboResolution = 1280 * 1024;  //put max turbo resolution here, max possible size when turbo is on
-var MaxSquareResolution = 1024;
+var maxTotalResolution = 1280 * 1280; //put max 'low' mode resolution here, max possible size when low mode is on
+var maxTurboResolution = 1024 * 1024;  //put max resolution (other than 'low' mode) here
 
-//NOTE: it is possible that it could choose resolution values that exceed maxTotalResolution.  Adjust may be necessary, but this should be an unlikely occurrence.
+//NOTE: it is possible that it could choose resolution values that exceed maxTotalResolution.  Adjustment may be necessary, but this should be an unlikely occurrence.
 
 function ScaleUpMax(dimension, ratio) {
   return Math.round(((dimension*ratio)+32)/64)*64-64;
@@ -43,19 +40,21 @@ PLUGINS['IMAGE_INFO_BUTTONS'].push({
       //The rounding takes it to the nearest 64, which defines the resolutions available.  This will choose values that are not in the UI.
       width: ScaleUpMax(origRequest.width,ratio),
       height: ScaleUpMax(origRequest.height,ratio),
+      guidance_scale: Math.max(origRequest.guidance_scale,15), //Some suggest that higher guidance is desireable for img2img processing
       num_inference_steps: Math.min(parseInt(origRequest.num_inference_steps) + 50, 100),  //large resolutions combined with large steps can cause an error
       num_outputs: 1,
       //Using a new seed will allow some variation as it up-sizes; if results are not ideal, rerunning will give different results.
       seed: Math.floor(Math.random() * 10000000)  //Remove or comment-out this line to retain original seed when resizing
     })
     newTaskRequest.seed = newTaskRequest.reqBody.seed
-    newTaskRequest.reqBody.sampler = 'ddim'  //ensure img2img sampler change is properly reflected in log file
+    newTaskRequest.reqBody.sampler_name = 'ddim'  //ensure img2img sampler change is properly reflected in log file
     newTaskRequest.batchCount = 1  // assume user only wants one at a time to evaluate, if selecting one out of a batch
     newTaskRequest.numOutputsTotal = 1 // "
     //If you have a lower-end graphics card, the below will automatically disable turbo mode for larger images.
-    //Each person needs to test with different resolutions to find the limit of their card when using turbo mode.
-    if (newTaskRequest.reqBody.width * newTaskRequest.reqBody.height > maxTurboResolution) {  //put max turbo resolution here
-      newTaskRequest.reqBody.turbo = false;
+    //Each person needs to test with different resolutions to find the limit of their card when using Balanced or modes other than 'low'.
+    if (newTaskRequest.reqBody.width * newTaskRequest.reqBody.height > maxTurboResolution) {  //put max normal resolution here
+      //newTaskRequest.reqBody.turbo = false;
+      newTaskRequest.reqBody.vram_usage_level = 'low';
     }
     delete newTaskRequest.reqBody.mask
     createTask(newTaskRequest)
