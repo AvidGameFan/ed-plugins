64c2f78b31a0c7a40bd5f404b5c075f30fe62d6e
Gary Wolfe <avidgamefan@yahoo.com>
2024-03-18 22:32:34 -0400

    Remove controlnet and upscale settings
    Remove controlnet and upscale settings from makeverysimilarimages

--- makeverysimilarimages.plugin.js
+++ makeverysimilarimages.plugin.js
@@ -1,7 +1,7 @@
 /***
  * 
  * Make Very Similar Images Plugin for Easy Diffusion
- * v.0.9.1, last updated: 2/13/2024
+ * v.0.9.2, last updated: 3/18/2024
  * By Gary W.
  * 
  * Similar to the original "Make Similar Images" plugin to make images somewhat similar to the original,
@@ -56,6 +56,12 @@ function onMakeVerySimilarClick(origRequest, image) {
 
 //newTaskRequest.numOutputsTotal = 5
 //newTaskRequest.batchCount = 5
+
+delete newTaskRequest.reqBody.use_controlnet_model;
+delete newTaskRequest.reqBody.control_filter_to_apply;
+delete newTaskRequest.reqBody.control_image;
+
+delete newTaskRequest.reqBody.use_upscale; //if previously used upscaler, we don't want to automatically do it again
 
 delete newTaskRequest.reqBody.mask
 
